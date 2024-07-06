6b0cb1a39a51421c9842db07adc0168769b564a4
oct-sky-out <kms3335k@naver.com>
2024-07-07 00:26:19 +0900

    [20240706] minsu.kim : 회원가입 폼 css width 사이즈별 대응

--- src/page/SignUpPage/SignUpPage.tsx
+++ src/page/SignUpPage/SignUpPage.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const SignUpPage: React.FC = () => {
   return (
     <Grid fullWidth>
-      <Column lg={{ span: 8, offset: 4 }}>
+      <Column sm={{ span: 4, offset: 0 }} md={{ span: 8, offset: 0 }} lg={{ span: 8, offset: 4 }}>
         <SignUp />
       </Column>
     </Grid>
