ad381c6e713c7ded26d3d4b3259a377ffaaaafa9
oct-sky-out <kms3335k@naver.com>
2024-07-27 14:15:43 +0900

    동적 라우트 주의 해결

--- src/page/router.tsx
+++ src/page/router.tsx
@@ -5,7 +5,7 @@ import { App } from '@/App';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const lazyPageLoad = async (pageCompoent: string): Promise<{ Component: React.FC<any> }> => {
-  const component = await import(`./${pageCompoent}`);
+  const component = await import(`./${pageCompoent}/index.ts`);
 
   return { Component: component.default };
 };
